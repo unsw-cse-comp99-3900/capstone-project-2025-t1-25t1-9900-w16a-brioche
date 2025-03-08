272562b8ef6f65bf65cd6990554fdaac8834f304
ZaneMa <zanema528@gmail.com>
2025-03-09 10:22:27 +1100

    chore: setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-blue-400">
-        InvoiceFlow
-      </h1>
+      <div className="flex flex-col justify-center items-center h-screen gap-5">
+        <h1 className="text-3xl font-bold underline text-blue-400">
+          InvoiceFlow
+        </h1>
+        <Button>Shadcn Button</Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast("Event has been created", {
+              description: "Sunday, December 03, 2023 at 9:00 AM",
+              action: {
+                label: "Undo",
+                onClick: () => console.log("Undo"),
+              },
+            })
+          }
+        >
+          Show Toast
+        </Button>
+      </div>
+      <Toaster />
     </>
   );
 }
