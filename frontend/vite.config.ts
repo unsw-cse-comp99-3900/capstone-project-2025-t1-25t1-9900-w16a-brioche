1e73b593b6964d17d8716fa020a618a155e79e45
Zane <zanema528@gmail.com>
2025-03-09 12:04:01 +1100

    chore: set up react compiler (#1)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,19 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const ReactCompilerConfig = {
+  /* ... */
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
