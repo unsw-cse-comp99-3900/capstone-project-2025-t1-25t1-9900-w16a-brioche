486e60a620ace38eb552078761a4e424f2a36e6c
ZaneMa <zanema528@gmail.com>
2025-03-09 20:53:06 +1100

    test: Remove intentional ESLint error file

--- frontend/src/invalid-eslint.ts
+++
@@ -1,9 +0,0 @@
-// This file intentionally causes an ESLint error for testing purposes.
-// The following code references an undefined variable, which should trigger an error.
-function testError(): void {
-  // @ts-ignore
-  const x: any = 5
-  x = 10 // This will trigger an ESLint error for reassigning a const
-}
-
-testError()
