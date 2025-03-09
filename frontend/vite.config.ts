e18f1f3c085e1a08177cb1809c5747f46802de16
ZaneMa <zanema528@gmail.com>
2025-03-09 11:24:09 +1100

    Revert "chore: set up react compiler"
    This reverts commit 66b2f9031925be795229dc5ba97733f9131faeaa.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,19 +2,9 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const ReactCompilerConfig = {
-  /* ... */
-};
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
