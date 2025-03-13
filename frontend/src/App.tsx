c02113ba0d2d0e38baa5c46c2a628ea17979c681
ZaneMa <zanema528@gmail.com>
2025-03-13 22:01:02 +1100

    chore: Remove unnecessary whitespace in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import { useEffect } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 function App() {
-  
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
