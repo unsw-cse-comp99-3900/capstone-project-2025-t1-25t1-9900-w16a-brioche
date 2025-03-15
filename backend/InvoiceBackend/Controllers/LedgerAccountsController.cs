9c2fda8649d711d5331e43cc4397b32e7cfa8cb3
Pan-y928 <80801834+Pan-y928@users.noreply.github.com>
2025-03-16 00:56:26 +1100

    Completed the APIs related to invoice creation. (#31)

---
+++ backend/InvoiceBackend/Controllers/LedgerAccountsController.cs
@@ -0,0 +1,64 @@
+﻿using InvoiceBackend.Helpers;
+using InvoiceBackend.Services.ReckonApiService;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceBackend.Controllers
+{
+    [ApiController]
+    [Route("api/ledgeraccounts")]
+    [EnableCors("AllowAll")]
+    public class LedgerAccountsController : ControllerBase
+    {
+        private readonly IReckonApiService _apiService;
+
+        public LedgerAccountsController(IReckonApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpGet("{bookId}")]
+        public async Task<IActionResult> GetLedgerAccounts(string bookId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, "ledgeraccounts", HttpMethod.Get);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpGet("{bookId}/{ledgerAccountId}")]
+        public async Task<IActionResult> GetLedgerAccountById(string bookId, string ledgerAccountId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"ledgeraccounts/{ledgerAccountId}", HttpMethod.Get);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpPost("{bookId}")]
+        public async Task<IActionResult> CreateLedgerAccount(string bookId, [FromBody] object ledgerAccountRequestBody)
+        {
+            string requestBody = System.Text.Json.JsonSerializer.Serialize(ledgerAccountRequestBody);
+
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, "ledgeraccounts", HttpMethod.Post, requestBody);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpDelete("{bookId}/{ledgerAccountId}")]
+        public async Task<IActionResult> DeleteLedgerAccount(string bookId, string ledgerAccountId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"ledgeraccounts/{ledgerAccountId}", HttpMethod.Delete);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpPut("{bookId}/{ledgerAccountId}")]
+        public async Task<IActionResult> UpdateLedgerAccount(string bookId, string ledgerAccountId, [FromBody] object ledgerAccountRequestBody)
+        {
+            string requestBody = System.Text.Json.JsonSerializer.Serialize(ledgerAccountRequestBody);
+
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"ledgeraccounts/{ledgerAccountId}", HttpMethod.Put, requestBody);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+    }
+}
