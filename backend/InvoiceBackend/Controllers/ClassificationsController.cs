62bd27f85a9d668f2a939c09501cb9d5a7ad63d4
Pan-y928 <80801834+Pan-y928@users.noreply.github.com>
2025-03-16 00:56:26 +1100

    Completed the APIs related to invoice creation. (#31)

---
+++ backend/InvoiceBackend/Controllers/ClassificationsController.cs
@@ -0,0 +1,64 @@
+﻿using InvoiceBackend.Helpers;
+using InvoiceBackend.Services.ReckonApiService;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceBackend.Controllers
+{
+    [ApiController]
+    [Route("api/classifications")]
+    [EnableCors("AllowAll")]
+    public class ClassificationsController : ControllerBase
+    {
+        private readonly IReckonApiService _apiService;
+
+        public ClassificationsController(IReckonApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpGet("{bookId}")]
+        public async Task<IActionResult> GetClassifications(string bookId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, "classifications", HttpMethod.Get);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpGet("{bookId}/{classificationId}")]
+        public async Task<IActionResult> GetClassificationById(string bookId, string classificationId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"classifications/{classificationId}", HttpMethod.Get);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpPost("{bookId}")]
+        public async Task<IActionResult> CreateClassification(string bookId, [FromBody] object classificationRequestBody)
+        {
+            string requestBody = System.Text.Json.JsonSerializer.Serialize(classificationRequestBody);
+
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, "classifications", HttpMethod.Post, requestBody);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpDelete("{bookId}/{classificationId}")]
+        public async Task<IActionResult> DeleteClassification(string bookId, string classificationId)
+        {
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"classifications/{classificationId}", HttpMethod.Delete);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+
+        [HttpPut("{bookId}/{classificationId}")]
+        public async Task<IActionResult> UpdateClassification(string bookId, string classificationId, [FromBody] object classificationRequestBody)
+        {
+            string requestBody = System.Text.Json.JsonSerializer.Serialize(classificationRequestBody);
+
+            HttpResponseMessage response = await _apiService.CallApiAsync(bookId, $"classifications/{classificationId}", HttpMethod.Put, requestBody);
+
+            return await ApiResponseHelper.HandleApiResponse(response);
+        }
+    }
+}
