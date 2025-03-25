ba665a3565f3a783e16e5861ec1f3f4fa71bdde5
Yan Pan <736056010@qq.com>
2025-03-25 23:03:14 +1100

    add ClerkAuthService backend

---
+++ backend/InvoiceBackend/Services/ClerkAuthService/ClerkAuthService.cs
@@ -0,0 +1,45 @@
+﻿using Clerk.BackendAPI.Helpers.Jwks;
+
+namespace InvoiceBackend.Services.ClerkAuthService
+{
+    public class ClerkAuthService: IClerkAuthService
+    {
+        private readonly string _secretKey;
+
+        public ClerkAuthService(IConfiguration configuration)
+        {
+            _secretKey = configuration["Clerk:SecretKey"];
+        }
+
+        public async Task<bool> VerifyTokenAsync(HttpRequest request)
+        {
+
+                var options = new AuthenticateRequestOptions(
+                    secretKey: _secretKey,
+                    authorizedParties: new string[] { "http://localhost:5173" }
+                );
+
+                var requestState = await AuthenticateRequest.AuthenticateRequestAsync(request, options);
+
+                return requestState.IsSignedIn();
+        }
+
+        // doesnt work
+        public async Task<string?> GetUserIdAsync(HttpRequest request)
+        {
+            var options = new AuthenticateRequestOptions(
+                secretKey: _secretKey,
+                authorizedParties: new string[] { "http://localhost:5173" }
+            );
+
+            var requestState = await AuthenticateRequest.AuthenticateRequestAsync(request, options);
+
+            if (!requestState.IsSignedIn())
+            {
+                return null;
+            }
+            var userId = requestState.Claims?.FindFirst("sub")?.Value;
+            return requestState.Claims.FindFirst("sub")?.Value; // wrong way
+        }
+    }
+}
