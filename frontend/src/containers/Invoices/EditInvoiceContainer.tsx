9c3ef1e72115e0389247460c6b4fcd1beb984dff
Xin Pi <z5470908@ad.unsw.edu.au>
2025-04-03 21:46:24 +1100

    reverted the isLoadingIvoice version

--- frontend/src/containers/Invoices/EditInvoiceContainer.tsx
+++ frontend/src/containers/Invoices/EditInvoiceContainer.tsx
@@ -294,7 +294,11 @@ const EditInvoiceContainer: React.FC = () => {
   }
 
   if (isLoadingInvoice) {
-    return <div>Loading...</div>
+    return (
+      <div className="flex items-center justify-center h-64">
+        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
+      </div>
+    )
   }
 
   return (
