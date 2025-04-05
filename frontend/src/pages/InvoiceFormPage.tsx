11a17dace3057ba2521f8bb61cc6300a9e2b9217
ZaneMa <zanema528@gmail.com>
2025-04-05 17:20:55 +1100

    Refactor: remove unused invoice container components and their associated pages

--- frontend/src/pages/InvoiceFormPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import InvoiceFormContainer from "@/containers/Invoice/InvoiceFormContainer"
-
-const InvoiceFormPage: React.FC = () => {
-  return <InvoiceFormContainer />
-}
-
-export default InvoiceFormPage
