509726cec658ab2584dc42da7d11ae7aaf2d3e71
Peter Pan <736056010@qq.com>
2025-04-06 22:21:56 +1000

    change BookPage and ReckonCallbackPage components, integrate Reckon OAuth callback functionality, and configure HTTPS in Vite

--- frontend/src/pages/BookPage.tsx
+++ frontend/src/pages/BookPage.tsx
@@ -1,16 +1,16 @@
 import React from "react"
-import CustomerBookContainer from "@/containers/Customers/CustomerBookContainer"
 import GridPatternOverlay from "@/components/common/GridPatternOverlay"
+import BookContainer from "@/containers/Customers/BookContainer"
 
-const CustomerBookPage: React.FC = () => {
+const BookPage: React.FC = () => {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <GridPatternOverlay />
       <div className="relative z-10 w-full max-w-7xl mx-auto px-4 ">
-        <CustomerBookContainer />
+        <BookContainer />
       </div>
     </div>
   )
 }
 
-export default CustomerBookPage
+export default BookPage
